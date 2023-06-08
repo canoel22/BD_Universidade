06584ac59502d905b5fbf8b1d30e254d9570a635
canoel22 <carine.noel2003@gmail.com>
2023-06-08 00:01:11 -0300

    cadastros ok

--- Cadastro/Cadastros.py
+++ Cadastro/Cadastros.py
@@ -13,7 +13,7 @@ def CadastroSetor(conexao, cur):
     codigo = input("Código: ") #Recebe o código do setor
     
     if(VerificaSetor(cur, codigo) == 0): #Verifica se o setor já foi cadastrado (0 = SIM)
-        print("\nERRO: Código já cadastrado!") #mensagem de erro pra codigo já cadastrado
+        print("\nERRO: Código já cadastrado!") #Mensagem de erro pra codigo já cadastrado
         input("Pressione ENTER para voltar.")
         return
         
@@ -27,22 +27,141 @@ def CadastroSetor(conexao, cur):
     input("Pressione ENTER para voltar.")
 
 
-def CadastroCurso(conexao, cur):
-    pass
+#--------------------- cadastro de um curso --------------------------------#
 
+def CadastroCurso(conexao, cur):
+    os.system('clear') #Limpa a tela
+    print("-> Cadastro de curso <-\n")
+    print("Por favor, informe:\n")
+    
+    nome = input("Nome: ") #Recebe o nome
+    codigo = input("Código: ") #Recebe o código do curso
+    ano = input("Ano de início: ") # Recebe o ano de início
+    
+    if(VerificaCurso(cur, codigo) == 0): #Verifica se o curso já foi cadastrado (0 = SIM)
+        print("\nERRO: Código já cadastrado!") #Mensagem de erro pra codigo já cadastrado
+        input("Pressione ENTER para voltar.")
+        return
+        
+    sql = "INSERT INTO cursos (cod_setor, nome, ano_inicio) VALUES ( %s, %s, %s)" #Insere na tabela setores
+    data = (codigo, nome, ano) #Utiliza os dados do insert
+    
+    cur.execute(sql, data) #Executa o comando(sql+data)
+    conexao.commit() #salva a alteração no banco
+
+    print("\nCadastrado com sucesso!")
+    input("Pressione ENTER para voltar.")
+
+
+#--------------------- cadastro de um funcionario --------------------------------#
 
 def CadastroFunc(conexao, cur):
-    pass
+    os.system('clear') #Limpa a tela
+    print("-> Cadastro de funcionário <-\n")
+    print("Por favor, informe:\n")
+    
+    nome = input("Nome: ") #Recebe o nome
+    cpf = input("CPF: ") #Recebe o código do curso
+    endereco = input("Endereço: ") # Recebe o endereço
+    salario = input("Salário: ") # Recebe o salário
+    cod_setor = input("Código do setor: ") # Recebe o setor
+    
+    if(VerificaFunc(cur, cpf) == 0): #Verifica se o curso já foi cadastrado (0 = SIM)
+        print("\nERRO: CPF já cadastrado!") #Mensagem de erro pra codigo já cadastrado
+        input("Pressione ENTER para voltar.")
+        return
+    
+    if(VerificaSetor(cur, cod_setor) == -1): #Verifica se o setor já foi cadastrado (-1 = NÃO)
+        print("\nERRO: Setor não cadastrado!") #Mensagem de erro pra setor não encontrado
+        input("Pressione ENTER para voltar.")
+        return
+        
+    sql = "INSERT INTO funcionarios (cpf, nome, endereco, salario, cod_setor) VALUES ( %s, %s, %s, %s, %s)" #Insere na tabela setores
+    data = (cpf, nome, endereco, salario, cod_setor) #Utiliza os dados do insert
 
+    cur.execute(sql, data) #Executa o comando(sql+data)
+    conexao.commit() #salva a alteração no banco
+
+    print("\nCadastrado com sucesso!")
+    input("Pressione ENTER para voltar.")
+
+
+#--------------------- cadastro de um professor --------------------------------#
 
 def CadastroProf(conexao, cur):
-    pass
+    os.system('clear') #Limpa a tela
+    print("-> Cadastro de professor <-\n")
+    print("Por favor, informe:\n")
+    
+    nome = input("Nome: ") #Recebe o nome
+    cpf = input("CPF: ") #Recebe o código do curso
+    telefone = input("Telefone: ") #Recebe o telefone
+    endereco = input("Endereço: ") # Recebe o endereço
+    salario = input("Salário: ") # Recebe o salário
+    data_contratacao = input("Data de contratação: ") #Recebe a data de contratação
+    cod_curso = input("Código do curso: ") # Recebe o setor
+    
+    if(VerificaFunc(cur, cpf) == 0): #Verifica se o curso já foi cadastrado (0 = SIM)
+        print("\nERRO: CPF já cadastrado!") #Mensagem de erro pra codigo já cadastrado
+        input("Pressione ENTER para voltar.")
+        return
+    
+    if(VerificaCurso(cur, cod_curso) == -1): #Verifica se o curso já foi cadastrado (-1 = NÃO)
+        print("\nERRO: Curso não cadastrado!") #Mensagem de erro pra curso não encontrado
+        input("Pressione ENTER para voltar.")
+        return
+        
+    sql = "INSERT INTO professores (cpf, nome, telefone, endereco, salario, data_contratacao, ativo, cod_curso) VALUES ( %s, %s, %s, %s, %s, %s, %s, %s)" #Insere na tabela setores
+    data = (cpf, nome, endereco, salario, data_contratacao, "Ativo", cod_curso) #Utiliza os dados do insert
+    
+    cur.execute(sql, data) #Executa o comando(sql+data)
+    conexao.commit() #salva a alteração no banco
 
+    print("\nCadastrado com sucesso!")
+    input("Pressione ENTER para voltar.")
+
+
+#--------------------- cadastro de um aluno --------------------------------#
 
 def CadastroAluno(conexao, cur):
-    pass
+    os.system('clear') #Limpa a tela
+    print("-> Cadastro de alunos <-\n")
+    print("Por favor, informe:\n")
+    
+    nome = input("Nome: ") #Recebe o nome
+    cpf = input("CPF: ") #Recebe o código do curso
+    telefone = input("Telefone: ") #Recebe o telefone
+    endereco = input("Endereço: ") # Recebe o endereço
 
+    
+    if(VerificaFunc(cur, cpf) == 0): #Verifica se o curso já foi cadastrado (0 = SIM)
+        print("\nERRO: CPF já cadastrado!") #Mensagem de erro pra codigo já cadastrado
+        input("Pressione ENTER para voltar.")
+        return
+        
+    sql = "INSERT INTO alunos (cpf, nome, telefone, endereco, ativo) VALUES ( %s, %s, %s, %s, %s)" #Insere na tabela setores
+    data = (cpf, nome, endereco, "Ativo") #Utiliza os dados do insert
+    
+    cur.execute(sql, data) #Executa o comando(sql+data)
+    conexao.commit() #salva a alteração no banco
+
+    print("\nCadastrado com sucesso!")
+    input("Pressione ENTER para voltar.")
+
+
+#--------------------- cadastro de um aluno --------------------------------#
 
 def CadastroDisc(conexao, cur):
-    pass
+    os.system('clear') #Limpa a tela
+    print("-> Cadastro de disciplina <-\n")
+    print("Por favor, informe:\n")
+    
+    cod_disc = input("Código da disciplina: ") # Recebe a disciplina
+    nome = input("Nome: ")
+    cpfProf = input("CPF do professor:")
+
+    if(VerificaProf(cur, cpfProf) == -1): #Verifica se o setor já foi cadastrado (-1 = NÃO)
+        print("\nERRO: Setor não cadastrado!") #Mensagem de erro pra setor não encontrado
+        input("Pressione ENTER para voltar.")
+        return
 
--- Verificacoes.py
+++ Verificacoes.py
@@ -2,7 +2,7 @@
 
 def VerificaSetor(cur, codigo):
     cur.execute("SELECT cod_setor FROM setores") #select em todos os codigos cadastrados
-    codigos_cadastrados = cur.fetchall() #Recebe todos os codgigos cadastrados
+    codigos_cadastrados = cur.fetchall() #Recebe todos os codigos cadastrados
     for linha in codigos_cadastrados: 
         if(linha[0] == codigo): #Verifica se o código já está no banco de dados
             return 0 #Retorna 0 se estiver
@@ -12,3 +12,34 @@ def VerificaSetor(cur, codigo):
 
 #--------------------- verifica se um curso já foi cadastrado --------------------------------#
 
+def VerificaCurso(cur, codigo):
+    cur.execute("SELECT cod_curso FROM cursos") #select em todos os codigos cadastrados
+    codigos_cadastrados = cur.fetchall() #Recebe todos os codigos cadastrados
+    for linha in codigos_cadastrados: 
+        if(linha[0] == codigo): #Verifica se o código já está no banco de dados
+            return 0 #Retorna 0 se estiver
+    else:
+        return -1 #Retorna -1 se não estiver
+    
+    
+#--------------------- verifica se um funcionario já foi cadastrado --------------------------------#
+
+def VerificaFunc(cur, cpf):
+    cur.execute("SELECT cpf FROM funcionarios") #select em todos os cpf cadastrados
+    cpf_cadastrados = cur.fetchall() #Recebe todos os cpf cadastrados
+    for linha in cpf_cadastrados: 
+        if(linha[0] == cpf): #Verifica se o cpf já está no banco de dados
+            return 0 #Retorna 0 se estiver
+    else:
+        return -1 #Retorna -1 se não estiver
+    
+#--------------------- verifica se um professir já foi cadastrado --------------------------------#
+
+def VerificaProf(cur, cpfProf):
+    cur.execute("SELECT cpf FROM professores") #select em todos os cpf cadastrados
+    cpf_cadastrados = cur.fetchall() #Recebe todos os cpf cadastrados
+    for linha in cpf_cadastrados: 
+        if(linha[0] == cpfProf): #Verifica se o cpf já está no banco de dados
+            return 0 #Retorna 0 se estiver
+    else:
+        return -1 #Retorna -1 se não estiver
