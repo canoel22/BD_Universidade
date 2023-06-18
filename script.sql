6ca573dc2632551ed37efb2e30b4bc102c5eeb98
canoel22 <carine.noel2003@gmail.com>
2023-06-18 16:18:43 -0300

    ajeitando o diagrama

--- script.sql
+++ script.sql
@@ -34,26 +34,28 @@ CREATE TABLE IF NOT EXISTS professores (
     CONSTRAINT cod_curso_fk1 FOREIGN KEY (cod_curso) REFERENCES cursos(cod_curso) ON DELETE CASCADE
 );
 
+CREATE TABLE IF NOT EXISTS disciplinas (
+    cod_disciplina INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    cpf_professor INT(11) NOT NULL,
+    CONSTRAINT cpf_professor_fk FOREIGN KEY (cpf_professor) REFERENCES professores(cpf) ON DELETE CASCADE
+);
+
 CREATE TABLE IF NOT EXISTS alunos (
     cpf INT(11) PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(50) NOT NULL,
     telefone VARCHAR(11) NOT NULL,
     endereco VARCHAR(100) NOT NULL,
-    ativo BOOLEAN NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS disciplinas (
-    cod_disciplina INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(50) NOT NULL,
-    cpf_professor INT(11) NOT NULL,
-    CONSTRAINT cpf_professor_fk FOREIGN KEY (cpf_professor) REFERENCES professores(cpf) ON DELETE CAsCADE
+    ativo BOOLEAN NOT NULL,
+    cod_disciplina INT NOT NULL,
+    CONSTRAINT cod_disciplina_fk1 FOREIGN KEY (cod_disciplina) REFERENCES disciplinas(cod_disciplina) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS cursos_disciplinas (
     cod_curso INT NOT NULL,
     cod_disciplina INT NOT NULL,
-    CONSTRAINT cod_curso_fk2 FOREIGN KEY (cod_curso) REFERENCES cursos(cod_curso)  ON DELETE CAsCADE,
-    CONSTRAINT cod_disciplina_fk1 FOREIGN KEY (cod_disciplina) REFERENCES disciplinas(cod_disciplina) ON DELETE CAsCADE,
+    CONSTRAINT cod_curso_fk2 FOREIGN KEY (cod_curso) REFERENCES cursos(cod_curso)  ON DELETE CASCADE,
+    CONSTRAINT cod_disciplina_fk2 FOREIGN KEY (cod_disciplina) REFERENCES disciplinas(cod_disciplina) ON DELETE CASCADE,
     PRIMARY KEY (cod_curso, cod_disciplina)
 );
 
@@ -62,7 +64,7 @@ CREATE TABLE IF NOT EXISTS inscritos (
     cpf_aluno INT(11) NOT NULL,
     nota FLOAT NOT NULL DEFAULT 0,
     vez INT NOT NULL DEFAULT 1,
-    CONSTRAINT cod_disciplina_fk2 FOREIGN KEY (cod_disciplina) REFERENCES disciplinas(cod_disciplina),
+    CONSTRAINT cod_disciplina_fk3 FOREIGN KEY (cod_disciplina) REFERENCES disciplinas(cod_disciplina),
     CONSTRAINT cpf_aluno_fk FOREIGN KEY (cpf_aluno) REFERENCES alunos(cpf),
     PRIMARY KEY (cod_disciplina, cpf_aluno)
 );
